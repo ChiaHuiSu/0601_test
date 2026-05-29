c9b0d0929b048d4badd0fbd1ce9a577af5bd8595
ChiaHuiSu <chsu@pllab.cs.nthu.edu.tw>
2026-05-29 07:30:42 +0000

    init commit

--- vadd.cpp
+++ vadd.cpp
@@ -4,9 +4,7 @@
 
 // GPU kernel for vector addition
 __global__ void vec_add(double *A, double *B, double *C, int n) {
-    int id = blockDim.x * blockIdx.x + threadIdx.x;
-    if (id < n)
-        C[id] = A[id] + B[id];
+    // [TODO] Add kernel code here
 }
 
 int main(int argc, char *argv[]) {
@@ -26,15 +24,13 @@ int main(int argc, char *argv[]) {
     }
 
     // Device pointers
+    // [TODO] hipMalloc
     double *d_A, *d_B, *d_C;
-    hipMalloc((void**)&d_A, bytes);
-    hipMalloc((void**)&d_B, bytes);
-    hipMalloc((void**)&d_C, bytes);
+
 
     // Copy data from host to device
-    hipMemcpy(d_A, h_A, bytes, hipMemcpyHostToDevice);
-    hipMemcpy(d_B, h_B, bytes, hipMemcpyHostToDevice);
-    hipMemcpy(d_C, h_C, bytes, hipMemcpyHostToDevice);
+    // [TODO] hipMemcpy
+
 
     // Kernel launch configuration
     const int threads_per_block = 256;
@@ -44,7 +40,7 @@ int main(int argc, char *argv[]) {
     vec_add<<<blocks_in_grid, threads_per_block>>>(d_A, d_B, d_C, N);
 
     // Copy result back to host
-    hipMemcpy(h_C, d_C, bytes, hipMemcpyDeviceToHost);
+    // [TODO] hipMemcpy
 
     // Print Something
     for (int i = 0; i < 10; i ++) {
